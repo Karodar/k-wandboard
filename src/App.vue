451074aa0670d8b8e4d3f650b82027454696486b
Karodar <Karodar@users.noreply.github.com>
2022-12-30 18:37:52 +0300

    feat: added fps counter

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
+import { ref } from '@vue/runtime-core';
 import Scene from './components/Scene.vue';
+
+const currentFps = ref(0);
+const updateFps = (fps) => {
+  currentFps.value = fps;
+};
 </script>
 
 <template>
+  <span class="fps">{{ currentFps }}</span>
   <h1>UnMonopoly</h1>
 
-  <Scene />
+  <Scene @fps="updateFps($event)" />
 </template>
 
 <style scoped>
+.fps {
+  color: red;
+  font-size: 2rem;
+  font-weight: 600;
+  text-shadow: 1px 1px 2px dodgerblue;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
