ae7065e0d1f7b996c06193cb5349748efd3db451
Karodar <Karodar@users.noreply.github.com>
2022-12-30 13:29:48 +0300

    build (init): create init structure

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from '@vue/runtime-core';
+import {
+  Engine,
+  Scene,
+  FreeCamera,
+  Vector3,
+  MeshBuilder,
+  StandardMaterial,
+  Color3,
+  HemisphericLight,
+} from '@babylonjs/core';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
